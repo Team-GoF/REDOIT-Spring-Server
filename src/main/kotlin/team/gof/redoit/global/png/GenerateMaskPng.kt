f139389a6dae58835453f15875030619fc9d277c
KimTaeO <taeo0328@gmail.com>
2023-10-12 17:40:42 +0900

    자바 ImageIO를 통한 공백 구간지정

---
+++ src/main/kotlin/team/gof/redoit/global/png/GenerateMaskPng.kt
@@ -0,0 +1,48 @@
+package team.gof.redoit.global.png
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.multipart.MultipartFile
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import javax.imageio.ImageIO
+
+
+@Component
+class GenerateMaskPng {
+    private val log = LoggerFactory.getLogger(GenerateMaskPng::class.java)
+
+    fun execute(file: MultipartFile): ByteArray {
+        val image: BufferedImage = ImageIO.read(file.inputStream)
+        val argbImage = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_ARGB)
+        val g = argbImage.createGraphics()
+        g.drawImage(image, 0, 0, null)
+        g.dispose()
+
+        val width = argbImage.width
+        val height = argbImage.height
+
+        log.info("$width")
+        log.info("$height")
+
+        val startX = width / 6
+        val startY = height / 6
+        val endX = startX * 5
+        val endY = startY * 5
+
+        for(x in startX until  endX) {
+            for(y in startY until endY) {
+                val rgb = argbImage.getRGB(x, y)
+
+                val newRGB = (0 shl 24) or (rgb and 0x00FFFFFF)
+
+                argbImage.setRGB(x, y, newRGB)
+            }
+        }
+
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        ImageIO.write(argbImage, "png", byteArrayOutputStream)
+
+        return byteArrayOutputStream.toByteArray()
+    }
+}
