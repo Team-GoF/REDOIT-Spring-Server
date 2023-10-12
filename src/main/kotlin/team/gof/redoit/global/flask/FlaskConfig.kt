6b2fa6bd80d2b576874223766481c51c4ec39803
KimTaeO <taeo0328@gmail.com>
2023-10-12 20:01:53 +0900

    constructorBinding 어노테이션 추가

--- src/main/kotlin/team/gof/redoit/global/flask/FlaskConfig.kt
+++ src/main/kotlin/team/gof/redoit/global/flask/FlaskConfig.kt
@@ -1,7 +1,9 @@
 package team.gof.redoit.global.flask
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
+@ConstructorBinding
 @ConfigurationProperties("flask")
 class FlaskConfig(
     url: String
